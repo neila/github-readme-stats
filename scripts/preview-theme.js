36d34efa9ca19d48c0f5a27997531c7a2db840d4
SHØ <neila@users.noreply.github.com>
2023-02-22 18:41:50 +0900

    add env to workflow

--- scripts/preview-theme.js
+++ scripts/preview-theme.js
@@ -302,6 +302,7 @@ const themeNameAlreadyExists = (name) => {
  * Main function.
  */
 export const run = async (prNumber) => {
+  console.log(process.env.DRY_RUN);
   const dryRun = process.env.DRY_RUN === "true" || false;
   try {
     debug("Retrieve action information from context...");
